0ab5acbcab6b523a3c580de79b756b47f1d52958
abdnet <idouhammou.a@outlook.fr>
2023-12-28 15:41:17 +0000

    customers: test orders

---
+++ tests/customers/assert_customer_orders.sql
@@ -0,0 +1,8 @@
+-- Test pour vérifier que la colonne TOTAL_ORDERS correspond au nombre d'ordres dans
+-- la table orders
+{{ config(severity="warn") }}
+select c.id, c.total_orders, count(o.id) as orders_count
+from {{ ref("customers") }} c
+join {{ ref("orders") }} o on c.id = o.customer
+group by 1, 2
+having c.total_orders != orders_count
